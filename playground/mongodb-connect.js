069c46bc6ec92e32d2fae015e7a1b2f4d15d69d5
Eduardo H. Fritsche <edu.fritsche@gmail.com>
2018-03-13 21:32:40 -0300

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,16 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) return console.log('Unable to connect to MongoDB server');
+    console.log('Connected to MongoDB server');
+
+    db.collection('Todos').insertOne({
+        text: 'walk the dog',
+        completed: true
+    }, (err, result) => {
+        if(err) return console.log(err);
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.close();
+});
